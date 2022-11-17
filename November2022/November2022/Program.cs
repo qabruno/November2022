ab3f00488a78237fe317780087a71e9fe3add9eb
Bruno <bmacedo1987@gmail.com>
2022-11-17 21:23:06 +1300

    create time record test added

--- November2022/November2022/Program.cs
+++ November2022/November2022/Program.cs
@@ -9,6 +9,7 @@ driver.Manage().Window.Maximize();
 
 // launch turnup portal
 driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+Thread.Sleep(1000);
 
 // identify username textbox and enter valid username
 IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
@@ -34,4 +35,60 @@ else
     Console.WriteLine("Login failed, test failed.");
 }
 
+
+// navigate to Time and Material
+IWebElement administrationDropdown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a/span"));
+administrationDropdown.Click();
+Thread.Sleep(500);
+
+IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+tmOption.Click();
+
+// click on Create New button
+Thread.Sleep(1500);
+IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id='container']/p/a"));
+createNewButton.Click();
+Thread.Sleep(500);
+
+// select Time in the typecode dropdown
+IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]/span"));
+typeCodeDropdown.Click();
+
+IWebElement timeOption = driver.FindElement(By.XPath("//*[@id='TypeCode_listbox']/li[2]"));
+timeOption.Click();
+
+// enter code in the code textbox
+IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
+codeTextbox.SendKeys("November2022");
+
+// enter description in the description textbox
+IWebElement descriptionTextbox = driver.FindElement(By.Id("Description"));
+descriptionTextbox.SendKeys("November2022");
+
+// enter price in price per unit textbox
+IWebElement overlappingTag = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+overlappingTag.Click();
+
+IWebElement priceTextbox = driver.FindElement(By.Id("Price"));
+priceTextbox.SendKeys("12");
+
+// click on save button
+IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+Thread.Sleep(2000);
+
+// check if new time record has been created successfully
+IWebElement goToLastPageButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+goToLastPageButton.Click();                                   
+
+IWebElement newCode = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[last()]/td[1]"));
+
+if(newCode.Text == "November2022")
+{
+    Console.WriteLine("Time record created successfully.");
+}
+else
+{
+    Console.WriteLine("Time record hasn't been created successfully");
+}
 
